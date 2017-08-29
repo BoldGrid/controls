a5847991e9f6dbf2aaa77373c94d8b1fd4fe2eda
Rafael Ramos <rramo012@gmail.com>
2017-08-29 16:11:25 -0400

    Progress on color control

---
+++ src/controls/sass/compile.js
@@ -0,0 +1,100 @@
+var BOLDGRID = BOLDGRID || {};
+BOLDGRID.Sass = BOLDGRID.Sass || {};
+
+export class SassCompiler {
+
+	constructor( options ) {
+
+		// Singleton.
+		if ( this.instance ) {
+			return this.instance;
+		}
+
+		this.init( options );
+	}
+
+	init( options ) {
+		this.instance = this;
+
+		this.options = _.defaults( {
+			'enableLogging': false,
+			'workerURL': BOLDGRIDSass.WorkerUrl
+		}, options );
+
+		this.sassLibrary = require( 'sass.js/dist/sass.js' );
+		this.processing = false;
+		this.compileCount = 0;
+		this.log = {};
+
+		this.$window = $( window );
+		this.compileDone = $.Event( 'boldgrid_sass_compile_done' );
+		this.sassLibrary.setWorkerUrl( this.options.workerURL );
+		this.compiler = new this.sassLibrary( this.options.workerURL );
+		BOLDGRID.Sass = this;
+	}
+
+	/**
+	 * Setup a compile function.
+	 */
+	compile( scss, options ) {
+		options = options || {};
+
+		this.processing = true;
+
+		this.compileCount++;
+
+		this.resetCompiler();
+
+		if ( this.options.enableLogging ) {
+			this.log.startTime = new Date().getTime();
+		}
+
+		this.compiler.compile( scss, ( result ) => {
+			let data = {
+				result: result,
+				source: options.source
+			};
+
+			this.processing = false;
+
+			if ( 0 !== result.status ) {
+				console.error( result.formatted );
+			}
+
+			this.$window.trigger( this.compileDone, data );
+
+			this.outputLogs( result, scss );
+		} );
+	}
+
+	outputLogs( result, scss ) {
+		if ( this.options.enableLogging ) {
+			let difference;
+
+			this.log.endTime = new Date().getTime();
+			difference = this.log.endTime - this.log.startTime;
+
+			console.log( difference, ' milliseconds' );
+			console.log( result );
+			console.log( scss );
+		}
+	}
+
+	/*
+	 * After about 50 compiles error thrown on compiles.
+	 *
+	 * @since 1.0.0
+	 */
+	resetCompiler() {
+		if ( 50 < this.compileCount ) {
+			this.compileCount = 0;
+			this.sassLibrary.setWorkerUrl( this.options.workerURL );
+			if ( this.compiler ) {
+				this.compiler.destroy();
+				this.compiler = null;
+			}
+
+			this.compiler = new this.sassLibrary( this.options.workerURL );
+		}
+	}
+}
