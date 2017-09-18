f4048e9c0d5139b30beec969c989f85c54386ed7
Rafael Ramos <rramo012@gmail.com>
2017-09-18 15:07:20 -0400

    Fixing bug that allowed color transitions

--- src/controls/color/js/control.js
+++ src/controls/color/js/control.js
@@ -8,7 +8,6 @@ import { Generate } from './generate.js';
 import { Picker as ColorPicker } from '../../color-picker';
 import BrehautColorJs from 'color-js/color';
 
-
 var $ = jQuery,
 	$window = $( window ),
 	colorPalette = BOLDGRID.COLOR_PALETTE.Modify,
@@ -63,6 +62,7 @@ colorPalette.setupEvents = function() {
 	colorPalette.bind_help_section_visibility();
 	colorPalette.bind_help_link();
 	colorPalette.bindActiveColorClick();
+	colorPalette.bindTransitions();
 
 	// Action that occurs when color palette is compiled.
 	colorPalette.bind_compile_done();
@@ -1047,85 +1047,7 @@ colorPalette.pickerPostInit = function() {
  */
 colorPalette.bindActiveColorClick = function() {
 	self.$palette_control_wrapper.on( 'click', '.boldgrid-active-palette li', function( e ) {
-		var transitionColor,
-			backgroundColor,
-			$previewerBody,
-			timeStartedCompile,
-			$this = $( this ),
-			originalColor = $this.css( 'background-color' ),
-			isNeutral = false,
-			desiredDelay = 350,
-			transitionDistance = 0.4,
-			darknessThreshold = 0.5;
-
 		e.stopPropagation();
 		colorPalette.activate_color( null, $( this ), true );
-
-		if ( self.fadeEffectInProgress || ! e.originalEvent ) {
-			return;
-		}
-
-		// Get the previewer frame.
-		$previewerBody = colorPalette.getPreviewerBody();
-
-		if ( self.hasNeutral && $this.is( ':last-of-type' ) ) {
-			isNeutral = true;
-		}
-
-		// Calculate the color to transition to.
-		backgroundColor = BrehautColorJs( originalColor );
-		if ( backgroundColor.getLuminance() > darknessThreshold ) {
-			transitionColor = backgroundColor.darkenByAmount( transitionDistance );
-			transitionColor = transitionColor.toCSS();
-		} else {
-			transitionColor = backgroundColor.lightenByAmount( transitionDistance );
-			transitionColor = transitionColor.toCSS();
-		}
-
-		// Set color to transition to.
-		$this.css( 'background-color', transitionColor );
-		if ( isNeutral ) {
-			$this.closest( '.boldgrid-active-palette' ).attr( 'data-neutral-color', transitionColor );
-		}
-
-		// Enable transitions for the colors.
-		$previewerBody.addClass( 'color-palette-transitions' );
-
-		// Compile.
-		colorPalette.update_theme_option();
-
-		// Reset Color.
-		$this.css( 'background-color', originalColor );
-		if ( isNeutral ) {
-			$this.closest( '.boldgrid-active-palette' ).attr( 'data-neutral-color', originalColor );
-		}
-
-		timeStartedCompile = new Date().getTime();
-		self.fadeEffectInProgress = true;
-
-		$window.one( 'boldgrid_sass_compile_done', function() {
-			var timeout = 0,
-				duration = new Date().getTime() - timeStartedCompile;
-
-			// The compile to fade back in should trigger at a minimum time of desiredDelay.
-
-			// If the compile time exceeds the min than the the timeout will be 0.
-			if ( duration < desiredDelay ) {
-				timeout = desiredDelay;
-			}
-
-			// Wait for compile to finish then fade back in.
-			$window.one( 'boldgrid_sass_compile_done', function( event, data ) {
-				setTimeout( function() {
-					colorPalette.update_iframe( data );
-					setTimeout( function() {
-						$previewerBody.removeClass( 'color-palette-transitions' );
-						self.fadeEffectInProgress = false;
-					}, 250 );
-				}, timeout );
-			} );
-
-			colorPalette.update_theme_option( { source: 'color_palette_focus' } );
-		} );
 	} );
 };
