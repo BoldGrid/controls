903375c9c21f408d63bcf895f0d2308392687bba
Rafael Ramos <rramo012@gmail.com>
2017-10-26 12:36:02 -0400

    Adding box shadow control

---
+++ src/controls/box-shadow/control.js
@@ -0,0 +1,123 @@
+import { Direction } from '../direction';
+import { Switch } from '../switch';
+import { Picker as ColorPicker } from '../color-picker';
+import configs from './config.js';
+import './style.scss';
+
+export class BoxShadow extends Direction {
+
+	constructor( options ) {
+		super( options );
+		this.slidersLinked = false;
+
+		this.controlOptions = configs.controlOptions;
+		this.sliderConfig = configs.sliderConfig;
+		this.shadowType = '';
+		this.shadowColor = 'black';
+
+		this.switchControl = new Switch( {
+			'name': 'box-shadow-outline',
+			'label': 'Outline / Inset'
+		} );
+
+		this.colorPicker = new ColorPicker();
+	}
+
+	/**
+	 * Get the configs for the current slider.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param  {string} slider Slider name.
+	 * @return {object}        Slider configuration.
+	 */
+	getSliderConfig( slider ) {
+		return this.sliderConfig[ slider ];
+	}
+
+	/**
+	 * Create a control.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @return {jQuery} Control.
+	 */
+	render() {
+		let $control;
+
+		super.render();
+
+		this._setupOutlineSwitch();
+		this._setupColorPicker();
+
+		$control = this.$control.add( this.switchControl.$element );
+		$control = $control.add( this.colorPicker.$element );
+
+		return $control;
+	}
+
+	/**
+	 * Update the css used on a component.
+	 *
+	 * @since 1.0.0
+	 */
+	update() {
+		let data = this.getValues(),
+			name = this.controlOptions.control.name,
+			cssString = [];
+
+		cssString.push( this.shadowType );
+		cssString.push( data[ name + '-horizontal-position' ] + this.selectedUnit );
+		cssString.push( data[ name + '-vertical-position' ] + this.selectedUnit );
+		cssString.push( data[ name + '-blur-radius' ] + this.selectedUnit );
+		cssString.push( data[ name + '-spread-radius' ] + this.selectedUnit );
+		cssString.push( this.shadowColor );
+
+		cssString = cssString.join( ' ' );
+
+		this.$target.css( 'box-shadow', cssString );
+	}
+
+	/**
+	 * Setup display of the color picker.
+	 *
+	 * @since 1.0.0
+	 */
+	_setupColorPicker() {
+		let options = {
+			hide: false,
+			change: ( e, ui ) => {
+				this.shadowColor = ui.color.toString();
+				this.update();
+			}
+		};
+
+		this.colorPicker.init( false, options );
+	}
+
+	/**
+	 * Setup events and rendering for outline switch.
+	 *
+	 * @since 1.0.0
+	 */
+	_setupOutlineSwitch() {
+		this.switchControl.render();
+
+		this.switchControl.$input.on( 'change', () => {
+			this.shadowType = this.switchControl.isEnabled() ? 'inset' : '';
+			this.update();
+		} );
+	}
+
+	/**
+	 * Update css as the control fires updates.
+	 *
+	 * @since 1.0.0
+	 */
+	_bindSliderChange() {
+		this.$control.on( 'slide-change', () => this.update() );
+	}
+
+}
+
+export { BoxShadow as default };
---
+++ src/controls/switch/control.js
@@ -0,0 +1,37 @@
+import template from './template.html';
+import './style.scss';
+
+export class Switch {
+	constructor( options ) {
+		this.options = _.defaults( options || {}, {
+			name: _.random( 0, 10000 ),
+			label: 'Off / On'
+		} );
+
+		this.template = _.template( template );
+	}
+
+	/**
+	 * Create a switch and return the html.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @return {jQuery} Switch created.
+	 */
+	render() {
+		this.$element = $( this.template( this.options ) );
+		this.$input = this.$element.find( 'input' );
+
+		return this.$element;
+	}
+
+	/**
+	 * Determine whether or not the switch is "on".
+	 *
+	 * @since 1.0.0
+	 * @return {Boolean} Is the switch on?
+	 */
+	isEnabled() {
+		return this.$input.prop( 'checked' );
+	}
+}
