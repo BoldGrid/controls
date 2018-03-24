29c748cf9ff006e6e9b85fa6d994126b0b4e722c
Rafael Ramos <rramo012@gmail.com>
2018-03-24 08:46:19 -0400

    Adding weight, varient, family

--- src/controls/typography/control.js
+++ src/controls/typography/control.js
@@ -3,13 +3,19 @@ var $ = window.jQuery;
 import template from './template.html';
 import './style.scss';
 import { Slider } from '../slider';
+import { FontFamily } from './family';
 
 export class Control {
+
+	constructor() {
+		this.familyControl = new FontFamily();
+	}
 
 	render() {
 		const $template = $( template );
 
-		$template.find( 'font-family' ).replaceWith( this.fontSizeRender() );
+		$template.find( 'font-family' ).replaceWith( this.familyControl.render() );
+		$template.find( 'font-size' ).replaceWith( this.fontSizeRender() );
 		$template.find( 'line-height' ).replaceWith( this.lineHeightRender() );
 		$template.find( 'letter-spacing' ).replaceWith( this.letterSpacingRender() );
 
