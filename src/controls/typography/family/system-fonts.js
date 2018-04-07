779c26336a6c860be1fdff622428a7c69ab9822e
Rafael Ramos <rramo012@gmail.com>
2018-04-07 09:13:42 -0400

    System Fonts

---
+++ src/controls/typography/family/system-fonts.js
@@ -0,0 +1,50 @@
+export default [
+	{
+		name: 'Arial',
+		style: 'Arial, Helvetica, sans-serif'
+	},
+	{
+		name: 'Arial Black',
+		style: 'Arial Black, Gadget, sans-serif'
+	},
+	{
+		name: 'Comic Sans',
+		style: 'Comic Sans MS, Textile, cursive'
+	},
+	{
+		name: 'Courier',
+		style: '\'Courier New\', Courier, monospace'
+	},
+	{
+		name: 'Impact',
+		style: 'Impact, Charcoal, sans-serif'
+	},
+	{
+		name: 'Lucida Console',
+		style: '\'Lucida Console\', Monaco, monospace'
+	},
+	{
+		name: 'Lucida Sans Unicode',
+		style: '\'Lucida Sans Unicode\', \'Lucida Grande\', sans-serif'
+	},
+	{
+		name: 'Palatino Linotype',
+		style: '\'Palatino Linotype\', \'Book Antiqua\', Palatino, serif'
+	},
+	{
+		name: 'Tahoma',
+		style: 'Tahoma, Geneva, sans-serif'
+	},
+	{
+		name: 'Times New Roman',
+		style: '\'Times New Roman\', Times, serif'
+	},
+	{
+		name: 'Trebuchet MS',
+		style: '\'Trebuchet MS\', Helvetica, sans-serif'
+	},
+	{
+		name: 'Verdana',
+		style: 'Verdana, Geneva, sans-serif'
+	}
+];
