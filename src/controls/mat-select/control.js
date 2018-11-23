ca58f11c49aefde5bfec5ff5524d94d44eca75ba
Rafael Ramos <rramo012@gmail.com>
2018-11-23 11:57:15 -0500

    Feature, icons to prepend mat select options

--- src/controls/mat-select/control.js
+++ src/controls/mat-select/control.js
@@ -3,13 +3,21 @@ var $ = window.jQuery;
 import template from './template.html';
 import './style.scss';
 import { MDCSelect } from '@material/select';
+import { MDCSelectIcon } from '@material/select/icon';
 
 export class Control {
 	constructor( options ) {
 		this.options = _.defaults( options || {}, {
 			name: _.random( 0, 10000 ),
 			label: 'My Select',
-			selected: null
+			icon: '',
+			selected: null,
+
+			/*
+			 * Format - Example: {'value': 'image.png'}
+			 * This will add an icon for individual options.
+			 */
+			iconOption: null
 		} );
 
 		this.template = _.template( template );
@@ -24,11 +32,41 @@ export class Control {
 	 */
 	render() {
 		this.$element = $( this.template( this.options ) );
-		this.$input = this.$element.find( 'select' );
+		this.$input = this.$element.find( 'select, input' );
+		this.$icon = this.$element.find( '.mdc-select__icon' );
 
-		new MDCSelect( this.$element.find( '.mdc-select' )[0] );
+		this.select = new MDCSelect( this.$element.find( '.mdc-select' )[0] );
+		this.icon = this.options.icon ? new MDCSelectIcon( this.$icon[0] ) : null;
+
+		if ( this.options.iconOption )  {
+			this.setupIconToggle();
+		}
 
 		return this.$element;
+	}
+
+	/**
+	 * Setup binding change event to toggle icons.
+	 *
+	 * @since 1.0.0
+	 */
+	setupIconToggle() {
+		this.toggleIcons();
+		this.select.listen( 'MDCSelect:change', () => this.toggleIcons() );
+	}
+
+	/**
+	 * Toggle the visibility of the icons.
+	 *
+	 * @since 1.0.0
+	 */
+	toggleIcons() {
+		let value = this.getValue();
+		if ( this.options.iconOption[ value ] ) {
+			this.$icon.attr( 'src', this.options.iconOption[ value ] ).show();
+		} else {
+			this.$icon.hide();
+		}
 	}
 
 	/**
