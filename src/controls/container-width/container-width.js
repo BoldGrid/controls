c7ba25fe3a25f273aab9e141bee0c3f2d9b3ed69
James Rosado <jamesros@boldgrid.com>
2022-02-02 11:18:14 -0500

    add defaults, remove %

--- src/controls/container-width/container-width.js
+++ src/controls/container-width/container-width.js
@@ -10,11 +10,40 @@ export class ContainerWidth extends MultiSlider {
 				name: 'container-width',
 				units: {
 					default: 'px',
-					enabled: [ 'px', '%' ]
+					enabled: [ 'px' ]
 				},
 				sliders: [
 					{ name: 'width', label: 'Width', cssProperty: 'width' },
-					{ name: 'max-width', label: 'Max Width', cssProperty: 'max-width' }
+					{ name: 'maxWidth', label: 'Max Width', cssProperty: 'max-width' }
+				],
+				defaults: [
+					{
+						media: [ 'base', 'desktop', 'large' ],
+						unit: 'px',
+						isLinked: false,
+						values: {
+							width: 1170,
+							maxWidth: 1170
+						}
+					},
+					{
+						media: [ 'tablet' ],
+						unit: 'px',
+						isLinked: false,
+						values: {
+							width: 970,
+							maxWidth: 970
+						}
+					},
+					{
+						media: [ 'phone' ],
+						unit: 'px',
+						isLinked: false,
+						values: {
+							width: 750,
+							maxWidth: 750
+						}
+					}
 				]
 			},
 			slider: {
@@ -22,12 +51,7 @@ export class ContainerWidth extends MultiSlider {
 					min: 0,
 					max: 3840,
 					step: 5
-				},
-				'%': {
-					min: 0,
-					max: 100,
-					step: 1
-				},
+				}
 			}
 		};
 	}
